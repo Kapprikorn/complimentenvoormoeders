9ad5d4b68d2c489b1ba2b083920f795a99321f16
Kapprikorn <122310945+Kapprikorn@users.noreply.github.com>
2025-09-01 14:40:34 +0200

    adds compliments route

---
+++ src/app/pages/compliments/copmliments.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compliments',
+  imports: [],
+  templateUrl: './compliments.html',
+  styleUrl: './compliments.css'
+})
+export class Compliments {
+
+}
